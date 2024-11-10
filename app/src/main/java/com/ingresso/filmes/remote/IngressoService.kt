b3ac71de7a615035a7bedf9ab6313dc64c91da18
gedeoaraujo <gedeao.lemobs@gmail.com>
2024-11-10 18:06:54 -0300

    Encapsulando resposta para tratamento das requisições.

--- app/src/main/java/com/ingresso/filmes/remote/IngressoService.kt
+++ app/src/main/java/com/ingresso/filmes/remote/IngressoService.kt
@@ -1,10 +1,11 @@
 package com.ingresso.filmes.remote
 
 import com.ingresso.filmes.remote.responses.ItemsResponse
+import retrofit2.Response
 import retrofit2.http.GET
 
 interface IngressoService {
 
     @GET("events/coming-soon/partnership/desafio")
-    suspend fun getMovies(): ItemsResponse
+    suspend fun getMovies(): Response<ItemsResponse>
 }
