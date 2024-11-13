7a693a7ae75787a873f62d33f26af7429bb38854
gedeoaraujo <gedeao.lemobs@gmail.com>
2024-11-13 15:46:09 -0300

    Utilizando função mais apropriada.

--- app/src/main/java/com/ingresso/filmes/di/MainModule.kt
+++ app/src/main/java/com/ingresso/filmes/di/MainModule.kt
@@ -4,6 +4,7 @@ import com.ingresso.filmes.IngressoRepository
 import com.ingresso.filmes.MainViewModel
 import com.ingresso.filmes.remote.IngressoClient
 import com.ingresso.filmes.remote.IngressoService
+import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
@@ -11,5 +12,5 @@ val appModule = module {
         IngressoClient.createService(IngressoService::class.java)
     }
     single { IngressoRepository(get()) }
-    single { MainViewModel(get()) }
+    viewModel { MainViewModel(get()) }
 }
