30b883d2485a902a0c74b8153fc0bbb1b866a173
gedeoaraujo <gedeao.lemobs@gmail.com>
2024-11-13 15:46:47 -0300

    Separando views da MainActivity para um novo fragmento.

--- app/src/main/java/com/ingresso/filmes/MainActivity.kt
+++ app/src/main/java/com/ingresso/filmes/MainActivity.kt
@@ -2,19 +2,20 @@ package com.ingresso.filmes
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import com.ingresso.filmes.databinding.ActivityMainBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var dialog: AlertDialog
     private lateinit var binding: ActivityMainBinding
-    private val viewModel: MainViewModel by viewModel()
+    private lateinit var navController: NavController
+    private val viewModel by viewModel<MainViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,32 +36,13 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        dialog = AlertDialog.Builder(this)
-            .setTitle(getString(R.string.error_dialog_title))
-            .setPositiveButton(getString(R.string.error_dialog_button)) { dialog, _ ->
-                viewModel.loadMovies()
-                dialog.dismiss()
-            }
-            .setCancelable(false)
-            .create()
-
-        viewModel.loading.observe(this){ loading ->
-            binding.loading.changeVisibility(loading)
-            binding.moviesList.changeVisibility(loading.not())
+        navController = binding.navHostFragment.run {
+            getFragment<NavHostFragment>().navController
         }
+    }
 
-        viewModel.movies.observe(this){ movies ->
-            binding.moviesList.adapter = MovieListAdapter(movies)
-        }
-
-        viewModel.error.observe(this){ error ->
-            if (error.isServerError) dialog.setMessage(
-                getString(R.string.error_server)
-            )
-            else dialog.setMessage(error.message)
-
-            if (dialog.isShowing.not()) dialog.show()
-        }
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
 
---
+++ app/src/main/java/com/ingresso/filmes/MoviesList.kt
@@ -0,0 +1,62 @@
+package com.ingresso.filmes
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.ingresso.filmes.databinding.MoviesListBinding
+import org.koin.androidx.viewmodel.ext.android.activityViewModel
+
+class MoviesList: Fragment() {
+
+    private lateinit var dialog: AlertDialog
+    private lateinit var binding: MoviesListBinding
+    private val viewModel by activityViewModel<MainViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = MoviesListBinding.inflate(
+            inflater, container, false
+        )
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        dialog = AlertDialog.Builder(requireContext())
+            .setTitle(getString(R.string.error_dialog_title))
+            .setPositiveButton(getString(R.string.error_dialog_button)) { dialog, _ ->
+                viewModel.loadMovies()
+                dialog.dismiss()
+            }
+            .setCancelable(false)
+            .create()
+
+        viewModel.loading.observe(viewLifecycleOwner){ loading ->
+            binding.loading.changeVisibility(loading)
+            binding.moviesList.changeVisibility(loading.not())
+        }
+
+        viewModel.movies.observe(viewLifecycleOwner){ movies ->
+            binding.moviesList.adapter = MovieListAdapter(movies){ id ->
+                viewModel.setSelectedMovie(id)
+                findNavController().navigate(R.id.secondFragment)
+            }
+        }
+
+        viewModel.error.observe(viewLifecycleOwner){ error ->
+            if (error.isServerError) dialog.setMessage(
+                getString(R.string.error_server)
+            )
+            else dialog.setMessage(error.message)
+
+            if (dialog.isShowing.not()) dialog.show()
+        }
+    }
+}
