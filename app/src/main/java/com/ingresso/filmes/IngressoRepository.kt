e6068f01af332e7d44c206a9053dba09e8af325a
gedeoaraujo <gedeao.lemobs@gmail.com>
2024-11-13 21:09:13 -0300

    Salvando localmente filmes ao fazer a requisição.

--- app/src/main/java/com/ingresso/filmes/IngressoRepository.kt
+++ app/src/main/java/com/ingresso/filmes/IngressoRepository.kt
@@ -1,16 +1,21 @@
 package com.ingresso.filmes
 
+import com.ingresso.filmes.local.MovieDao
 import com.ingresso.filmes.remote.IngressoService
 import com.ingresso.filmes.remote.responses.MovieResponse
 
 class IngressoRepository(
-    private val service: IngressoService
+    private val service: IngressoService,
+    private val movieDao: MovieDao
 ) {
 
     suspend fun loadMovies(): List<MovieResponse> {
         val response = service.getMovies()
         val items = response.body()?.items
-        return items?.sortedBy { it.premiereDate?.localDate }.orEmpty()
+        val list = items?.sortedBy { it.premiereDate?.localDate }
+        val entities = list.orEmpty().map { it.toEntity() }
+        movieDao.insertMovies(entities)
+        return list.orEmpty()
     }
 
 }
