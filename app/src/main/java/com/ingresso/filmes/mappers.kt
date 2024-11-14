bd1554ed6ec458f81d203f6a6ab061dc0af704e4
gedeoaraujo <gedeao.lemobs@gmail.com>
2024-11-13 21:08:19 -0300

    Adicionando mapper para transformar a resposta em entidade.

---
+++ app/src/main/java/com/ingresso/filmes/mappers.kt
@@ -0,0 +1,22 @@
+package com.ingresso.filmes
+
+import com.ingresso.filmes.local.MovieEntity
+import com.ingresso.filmes.remote.responses.MovieResponse
+
+fun MovieResponse.toEntity(): MovieEntity {
+    return MovieEntity(
+        id = id,
+        title = title,
+        contentRating = contentRating,
+        duration = duration,
+        rating = rating,
+        synopsis = synopsis,
+        director = director,
+        distributor = distributor,
+        inPreSale = inPreSale,
+        localDate = premiereDate?.localDate.orEmpty(),
+        city = city,
+        imageUrl = images.map { it.url }.firstOrNull(),
+        genre = genres.first()
+    )
+}
