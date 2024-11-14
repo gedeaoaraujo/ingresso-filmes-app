dc39e1f79a27fd7d37dceeda1202d73486bc9461
gedeoaraujo <gedeao.lemobs@gmail.com>
2024-11-13 21:07:34 -0300

    Configurando injeção de dependencias para o banco de dados.

--- app/src/main/java/com/ingresso/filmes/di/MainModule.kt
+++ app/src/main/java/com/ingresso/filmes/di/MainModule.kt
@@ -2,15 +2,18 @@ package com.ingresso.filmes.di
 
 import com.ingresso.filmes.IngressoRepository
 import com.ingresso.filmes.MainViewModel
+import com.ingresso.filmes.local.AppDatabase
+import com.ingresso.filmes.local.MovieDao
 import com.ingresso.filmes.remote.IngressoClient
 import com.ingresso.filmes.remote.IngressoService
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
+    single<MovieDao> { AppDatabase.getDatabase(get()).movieDao() }
     single<IngressoService> {
         IngressoClient.createService(IngressoService::class.java)
     }
-    single { IngressoRepository(get()) }
+    single { IngressoRepository(get(), get()) }
     viewModel { MainViewModel(get()) }
 }
